514ccfdb0de6b6daf0e42d2a2a9acf189c387196
Matt McHughes <matt.mchughes@liveauctioneers.com>
2023-04-07 10:17:33 -0500

    first run

---
+++ unlock.js
@@ -0,0 +1,24 @@
+let unlockForm = document.getElementById("unlock-form");
+let audio = document.getElementById("audio");
+let coming = document.getElementById("coming-text");
+if (Date.now() > new Date('2023-04-21')) {
+    audio.style.display = 'flex';
+    unlockForm.style.display = 'none';
+    coming.style.display = 'none';
+    unlock.value = "";
+}
+
+unlockForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let unlock = document.getElementById("unlock");
+
+  if (unlock.value == "top ten state") {
+
+    audio.style.display = 'flex';
+    unlockForm.style.display = 'none';
+    coming.style.display = 'none';
+    unlock.value = "";
+   
+  }
+});
